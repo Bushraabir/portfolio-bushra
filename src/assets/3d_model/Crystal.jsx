184cfa18c74e541710a57c17910e294ddb69299a
Bushraabir <186773951+Bushraabir@users.noreply.github.com>
2025-07-28 14:42:03 +0600

    optimised and updated color palate

---
+++ src/assets/3d_model/Crystal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF('assets/3d_model/crystal.gltf')
+  return (
+    <group {...props} dispose={null} scale={[2, 2, 2]}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <lineSegments geometry={nodes.Object_2.geometry} material={materials.Material_0} />
+        <mesh geometry={nodes.Object_3.geometry} material={materials.Material_0} />
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('assets/3d_model/crystal.gltf')
